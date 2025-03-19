3e2ff77023d9ed8a5f0907036e48d0011948d028
moncapiten <moncapiten@gmail.com>
2025-03-19 17:11:10 +0100

    done some changes to newDataVisual, now you can plot 2 data series together, TO; GET HER

--- Code/Matlab/newDataVisual.m
+++ Code/Matlab/newDataVisual.m
@@ -1,21 +1,35 @@
 dataPosition = '../../Data/';
-filename = 'dataBode001';
+filename1 = 'data001';
+filename2 = 'data002';
 
-data = readmatrix(strcat(dataPosition, filename, '.txt'));
-tt = data(:, 1);
-ch1 = data(:, 2);
-ch2 = data(:, 3);
+data1 = readmatrix(strcat(dataPosition, filename, '.txt'));
+tt1 = data1(:, 1);
+ch11 = data1(:, 2);
+ch21 = data1(:, 3);
 
-t = tiledlayout(2, 1);
+data2 = readmatrix(strcat(dataPosition, filename2, '.txt'));
+tt2 = data2(:, 1);
+ch12 = data2(:, 2);
+ch22 = data2(:, 3);
 
-nexttile
-plot(tt, ch1, 'o', Color = '#0027BD');
+t = tiledlayout(2, 1, "TileSpacing","tight", "Padding","tight");
+
+ax1 = nexttile
+plot(tt1, ch11, 'o', Color = '#0027BD');
+hold on
+plot(tt1, ch21, 'v', Color = '#FF0000');
+hold off
 grid on
 grid minor
 
-nexttile
-plot(tt, ch1, 'o', Color = '#0027BD');
+ax2 = nexttile
+plot(tt2, ch12, 'o', Color = '#0027BD');
+hold on
+plot(tt2, ch22, 'v', Color = '#FF0000');
+hold off
 grid on
 grid minor
 
 title(t, 'Data Visualization')
+
+
